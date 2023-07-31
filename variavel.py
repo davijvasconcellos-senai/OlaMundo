c49990c0ac84b2cf621da400a6c8e1127148f7ce
Davi Vasconcellos <davijvasconcellos@gmail.com>
2023-07-31 11:36:07 -0300

    Alteração feita depois de ter clonado o projeto

--- variavel.py
+++ variavel.py
@@ -1,4 +1,5 @@
 nome = input("Qual é o seu nome?") # input é um função que permite perguntar algo ao usuário
 print(nome)
 print("Olá,",nome,". tudo bem com você?")
+print("Alteração depois de clonado")
 print(type(nome))
