4e6c79deb711ba293a1831f69e98d3bdd6142164
Davi Vasconcellos <davijvasconcellos@gmail.com>
2023-08-01 09:20:52 -0300

    Adicionado, mais variáveis para este arquivo e suas respectivas operações

--- variavel.py
+++ variavel.py
@@ -1,5 +1,62 @@
-nome = input("Qual é o seu nome?") # input é um função que permite perguntar algo ao usuário
-print(nome)
-print("Olá,",nome,". tudo bem com você?")
-print("Alteração depois de clonado")
-print(type(nome))
+#nome = input("Qual é o seu nome?") # input é um função que permite perguntar algo ao usuário
+#print(nome)
+
+#print("Alteração depois de clonado")
+
+#print(type(nome))
+
+a = 10
+b = 5.8
+c = "Rio de Janeiro"
+d = True
+
+print("a: ", a)
+print("b: ", b)
+print("c: ", c)
+print("d: ", d)
+
+print("Tipo da var a:", type(a)) # tipo inteiro (número inteiro)
+print("Tipo da var b:", type(b)) # tipo float (números reias)
+print("Tipo da var c:", type(c)) # tipo string (alfanumérico)
+print("Tipo da var d:", type(d)) # tipo boolean (True ou False)
+
+a = 20
+print("a: ", a)
+
+b = "São Paulo"
+print("b: ", b)
+
+print("Tipo da var a:", type(a))
+print("Tipo da var b:", type(b))
+
+a = input("Informe um número: ")
+b = input("Informe outro número: ")
+print("a: ", a, " - b: ", b)
+print("Tipo da var a:", type(a))
+print("Tipo da var b:", type(b))
+c = a + b # concatenou as stings de a e b
+print("c:", c)
+print("Tipo da var c:", type(c))
+d = int(a)
+print("d: ", d)
+print("Tipo da var d:", type(d))
+
+a = int(input("Informe um número: "))
+b = int(input("Informe outro número: "))
+print("a: ", a, " - b: ", b)
+print("Tipo da var a:", type(a))
+print("Tipo da var b:", type(b))
+
+c = a + b
+print("c:", c)
+print("Tipo da var c:", type(c))
+
+a = float(input("Informe um número: "))
+b = float(input("Informe outro número: "))
+print("a: ", a, " - b: ", b)
+print("Tipo da var a:", type(a))
+print("Tipo da var b:", type(b))
+
+c = a + b
+print("c:", c)
+print("Tipo da var c:", type(c))
